f3ae7c7c68ba4e7663b60abea88d7a9c05868879
Dan Navarro <dannavs@yahoo.com>
2026-01-06 11:38:27 -0800

    create custom hook initial fetch

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+
+
 const UseFetch = (url) => {
-    
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch.then((res) => res.json())
+            .then((data) => setData(data))
+    }, [])
+
+    return { data };
+
 }
 
 export default UseFetch
